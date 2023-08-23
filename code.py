d417c8fac35a1094d28b051a60388d7b15bd5482
root <root@ip-172-31-40-122.ap-south-1.compute.internal>
2023-08-23 17:21:14 +0000

    dakshna1

---
+++ code.py
@@ -0,0 +1,20 @@
+import requests
+
+# Replace 'YOUR_USERNAME' with your actual GitHub username
+username = 'YOUR_USERNAME'
+
+# GitHub API endpoint for user repositories
+url = f'https://api.github.com/users/{username}/repos'
+
+# Send a GET request to the API endpoint
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    repositories = response.json()
+    print(f"Repositories for user '{username}':")
+    for repo in repositories:
+        print(repo['name'])
+else:
+    print(f"Failed to retrieve repositories. Status code: {response.status_code}")
+
